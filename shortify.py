c6084381218d32cb78619fff2c6d09cebe821b2e
efazati <mohammad@efazati.org>
2017-01-23 17:23:29 +0330

    find url from text file

---
+++ shortify.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import re
+
+url_regex = re.compile(r"""(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>\[\]]+|\(([^\s()<>\[\]]+|(\([^\s()<>\[\]]+\)))*\))+(?:\(([^\s()<>\[\]]+|(\([^\s()<>\[\]]+\)))*\)|[^\s`!(){};:'".,<>?\[\]]))""")
+
+matches = []
+
+def process_match(m):
+    matches.append(m.group(0))
+    return '{{URL}}'
+
+
+with open(sys.argv[1], 'r') as need_shortify_file:
+	result = url_regex.sub(process_match, need_shortify_file.read())
+    print result
