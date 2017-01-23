bdfd3ee4f9fb4363b116fb95d43e996cc5dcb619
efazati <mohammad@efazati.org>
2017-01-23 17:26:52 +0330

    better command line

--- shortify.py
+++ shortify.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+import sys
 
 url_regex = re.compile(r"""(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>\[\]]+|\(([^\s()<>\[\]]+|(\([^\s()<>\[\]]+\)))*\))+(?:\(([^\s()<>\[\]]+|(\([^\s()<>\[\]]+\)))*\)|[^\s`!(){};:'".,<>?\[\]]))""")
 
@@ -9,7 +10,9 @@ def process_match(m):
     matches.append(m.group(0))
     return '{{URL}}'
 
-
-with open(sys.argv[1], 'r') as need_shortify_file:
-	result = url_regex.sub(process_match, need_shortify_file.read())
-    print result
+if len(sys.argv) == 1:
+    print "Please introduce file"
+else:
+    with open(sys.argv[1], 'r') as need_shortify_file:
+        result = url_regex.sub(process_match, need_shortify_file.read())
+        print result
